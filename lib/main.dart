8b746682c503eb766f4c7535951e79730702ca52
BurhanuddinBhopal <87414964+BurhanuddinBhopal@users.noreply.github.com>
2022-01-24 12:52:42 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    int days = 30;
+
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome to $days days of Flutter"),
+          ),
+        ),
+      ),
+    );
+  }
+}
